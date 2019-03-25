e3562633f9d2fce11df4d027b61d674883003f9d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-03-24 22:05:34 -0400

    add actual columns to user table migration

--- src/backend/lib/migrations/20190304010521_create_users_table.js
+++ src/backend/lib/migrations/20190304010521_create_users_table.js
@@ -1,8 +1,11 @@
 exports.up = (knex, Promise) => {
     return knex.schema.createTable('users', (table) => {
         table.increments().primary();
+        table.string('github_id').notNullable();
         table.string('username').notNullable();
-        table.string('password').notNullable();
+        table.string('avatar').defaultTo('https://cdn-images-1.medium.com/fit/c/200/200/1*_fyQhg7HIyLJbw-tdFBxXA.png');
+        table.string('email').notNullable();
+        table.string('oauth_type').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
