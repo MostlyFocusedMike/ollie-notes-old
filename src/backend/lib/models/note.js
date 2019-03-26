9345f1baba4a3420aa45aaedd44992869b794841
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-03-25 23:56:02 -0400

    [OLLIE-1] add user and note classes

---
+++ src/backend/lib/models/note.js
@@ -0,0 +1,30 @@
+const { Model } = require('objection');
+const knex = require('../knex');
+
+// Give the knex object to objection.
+Model.knex(knex);
+
+class Note extends Model {
+    static get tableName() {
+        return 'notes';
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['title'],
+
+            properties: {
+                id: { type: 'integer' },
+                user_id: { type: 'integer' },
+                title: { type: 'string' },
+                text: { type: 'string' },
+                oauth_type: { type: 'string' },
+                created_at: { type: 'string' },
+                updated_at: { type: 'string' },
+            },
+        };
+    }
+}
+
+module.exports = Note;
---
+++ src/backend/lib/models/user.js
@@ -0,0 +1,49 @@
+const Path = require('path');
+const { Model } = require('objection');
+const knex = require('../knex');
+
+// Give the knex object to objection.
+Model.knex(knex);
+
+class User extends Model {
+    static get tableName() {
+        return 'owners';
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: [
+                'username',
+                'email',
+                'oauth_type',
+            ],
+
+            properties: {
+                id: { type: 'integer' },
+                github_id: { type: 'integer' },
+                username: { type: 'string' },
+                avatar: { type: 'string' },
+                email: { type: 'string' },
+                oauth_type: { type: 'string' },
+                created_at: { type: 'string' },
+                updated_at: { type: 'string' },
+            },
+        };
+    }
+
+    static get relationMappings() {
+        return {
+            notes: {
+                relation: Model.HasManyRelation,
+                modelClass: Path.join(__dirname, 'note'),
+                join: {
+                    from: 'users.id',
+                    to: 'notes.user_id',
+                },
+            },
+        };
+    }
+}
+
+module.exports = User;
