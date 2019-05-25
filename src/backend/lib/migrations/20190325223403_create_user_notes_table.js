e8ce6b0fd735ea63e715a3e02dde5fba9d93ac6e
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-05-24 22:42:16 -0400

    [OLLIE-9] add new migration to make user_notes table

---
+++ src/backend/lib/migrations/20190325223403_create_user_notes_table.js
@@ -0,0 +1,15 @@
+exports.up = (knex) => {
+    return knex.schema.createTable('user_notes', (table) => {
+        table.increments().primary();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+        table.integer('note_id').unsigned().notNullable();
+        table.foreign('note_id').references('id').inTable('notes').onDelete('CASCADE');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = (knex) => {
+    return knex.schema.dropTable('user_notes');
+};
