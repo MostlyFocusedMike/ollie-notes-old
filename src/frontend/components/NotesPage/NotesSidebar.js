f8f81fc4e571893a68d55719514f6a66add1815c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-05-26 19:21:41 -0400

    [OLLIE-10] add sidebar and basic styles for visibility

--- src/frontend/components/NotesPage/NotesSidebar.js
+++ src/frontend/components/NotesPage/NotesSidebar.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NotesSidebar from './NotesSidebar';
 
 const NotesPage = (props) => {
-    const { username } = props.match.params;
-
     return (
-        <div id='notes-page'>
-            <h1>Notes for { username }</h1>
-            <NotesSidebar />
+        <div id='notes-sidebar'>
+            <h1>NOTES SIDEBAR</h1>
         </div>
     );
 };
