827be8a887adb36efdb154f3a3c6a3d02bc0c56d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-05-27 20:45:33 -0400

    [OLLIE-11] add listUserNoteTitles adapter

--- src/frontend/adapters/UserAdapter.js
+++ src/frontend/adapters/UserAdapter.js
@@ -1,24 +1,30 @@
 class UserAdapter {
     static getOne(username) {
-        const options = {
-            method: 'GET',
-            credentials: 'include', // fetch doesn't include cookies by default
-            headers: {
-                accepts: 'application/json',
-            },
-        };
-        return fetch(`${this.url}/${username}`, options)
+        return fetch(`${this.url}/${username}`, this.options)
             .then(r => r.json())
             .catch(console.log);
     }
 
     static list() {
-        return fetch(this.url)
+        return fetch(this.url, this.options)
+            .then(r => r.json())
+            .catch(console.log);
+    }
+
+    static listUserNoteTitles(username) {
+        return fetch(`${this.url}/${username}/note-titles`, this.options)
             .then(r => r.json())
             .catch(console.log);
     }
 }
 
 UserAdapter.url = '/api/v1/users';
+UserAdapter.options = {
+    method: 'GET',
+    credentials: 'include', // fetch doesn't include cookies by default
+    headers: {
+        accepts: 'application/json',
+    },
+};
 
 export default UserAdapter;
