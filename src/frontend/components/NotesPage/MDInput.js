0fcb2e8f4da6c580ab8cd1762d02df9a76f5b8c0
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-06-06 20:11:25 -0400

    [OLLIE-12] control the basic react form

--- src/frontend/components/NotesPage/MDInput.js
+++ src/frontend/components/NotesPage/MDInput.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MDInput = () => {
+    const [currentNote, setCurrentNote] = useState({
+        title: '',
+        text: '',
+    });
+
+    const handleChange = (e) => {
+        setCurrentNote({ [e.target.name]: e.target.value });
+    };
+
     return (
         <div id='md-input'>
             <form>
                 <label htmlFor='note-title'>Note Title: </label>
                 <input
                     id='note-title'
+                    name='title'
                     type='text'
+                    value={currentNote.title}
+                    onChange={handleChange}
                 />
                 <label htmlFor='note-text'>Text: </label>
                 <textarea
                     id='note-text'
+                    name='text'
+                    value={currentNote.text}
+                    onChange={handleChange}
                 />
             </form>
 
