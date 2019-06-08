efa8656494a2547297117d4e4ff470c0eab58922
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-06-08 16:33:22 -0400

    [OLLIE-13] add update method to note model

--- src/backend/models/note/index.js
+++ src/backend/models/note/index.js
@@ -4,6 +4,20 @@ class Note extends NoteDB {
     static async find(id) {
         return this.query().findById(id);
     }
+
+    $beforeUpdate() {
+        this.updated_at = new Date().toISOString();
+    }
+
+    static async update(note) {
+        if (!note.id) return false;
+        return this.query()
+            .update({
+                title: note.title,
+                text: note.text,
+            })
+            .where('id', note.id);
+    }
 }
 
 module.exports = Note;
